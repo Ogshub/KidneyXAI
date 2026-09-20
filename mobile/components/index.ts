601f62518b42545d6b50ef9eefdd26962766b011
thekaif <kaif01450@gmail.com>
2026-09-20 15:21:27 +0530

    Remove mobile app from main branch

--- mobile/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { Screen } from './Screen';
-export { AppText } from './AppText';
-export { AppButton } from './AppButton';
-export { Card } from './Card';
-export { Input } from './Input';
-export { LoadingView } from './LoadingView';
-export { ErrorView } from './ErrorView';
-export { Select } from './Select';
